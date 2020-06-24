ec053ea660ec8c2334d02c5505f8169a2ee85c9a
Quentin <quentin5555555555@gmail.com>
2020-06-24 16:22:21 +0200

    Modify objective paragraph

--- mail/contact_me.php
+++ mail/contact_me.php
@@ -19,7 +19,7 @@ $message = strip_tags(htmlspecialchars($_POST['message']));
 $to = 'contact.trichotillomanie@gmail.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
 $email_subject = "Formulaire de contact site:  $name";
 $email_body = "Vous avez reçu un nouveau message de votre formulaire de contact.\n\n"."Voici les détails:\n\nNom: $name\n\nEmail: $email_address\n\nTéléphone: $phone\n\nMessage:\n$message";
-$headers = "From: contact.trichotillomanie@gmail.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers = "From: contact-noreply@t-project.fr\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
 $headers .= "Reply-To: $email_address";   
 mail($to,$email_subject,$email_body,$headers);
 return true;         
